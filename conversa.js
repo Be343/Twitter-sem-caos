03a629bef10a535bdc0798455cd99feb2012d0f8
Be343 <147116049+Be343@users.noreply.github.com>
2024-03-02 13:11:15 -0300

    Update conversa.js

--- conversa.js
+++ conversa.js
@@ -40,7 +40,7 @@ function getData(){
             name = message_data["name"];
             message = message_data["message"];
             like = message_data["like"];
-            name_with_id =
+            name_with_tag =
               "<h4> " + name + "<img class= 'user_tick' src='tick.png'></h4>";
             message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
             like_button =
